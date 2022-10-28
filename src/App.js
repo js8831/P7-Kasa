065068d1527ff9ecf60cd9d3313d36f5258ec2f8
Jean Soulié <kalashh7@gmail.com>
2022-10-28 15:40:05 +0200

    fix : router + normalize

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import "./App.css";
+import About from "./pages/About/About";
+import NoMatch from "./pages/NoMatch/NoMatch";
+import Apartment from "./pages/Apartment/Apartment";
 
-function App() {
-  return <Home />;
+export default function App() {
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="apartment/:id" element={<Apartment />} />
+      <Route path="*" element={<NoMatch />} />
+    </Routes>
+  );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import "./normalize.css";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <App />
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
