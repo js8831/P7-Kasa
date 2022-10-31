94a3069500872a57ae0ad932c3d4ae473a896df0
Jean Soulié <kalashh7@gmail.com>
2022-10-31 12:17:45 +0100

    fix : replacement

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,6 @@
+/* import React from "react";
+import { useState } from "react";
+import arrowLeft from "../../assets/img/arrowLeft.svg";
+import arrowRight from "../../assets/img/arrowRight.svg"; */
+
+export default function Slider() {}
