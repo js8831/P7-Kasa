2a18208c7792e9964032d7c6285ee7fd7f60b5e3
Jean Soulié <kalashh7@gmail.com>
2022-11-01 12:13:48 +0100

    fix : router in one page

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./normalize.css";
 import "./index.scss";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import Apartment from "./pages/Apartment/Apartment";
+import About from "./pages/About/About";
+import NoMatch from "./pages/NoMatch/NoMatch";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Header />
-      <App />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="apartment/:productId" element={<Apartment />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   </React.StrictMode>
@@ -23,3 +31,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
