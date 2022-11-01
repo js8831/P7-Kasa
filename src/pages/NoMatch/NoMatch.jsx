09c7a7abafdbaa99f619c98693713dbc12b21d66
Jean Soulié <kalashh7@gmail.com>
2022-11-01 12:13:22 +0100

    feat : nomatch page

--- src/pages/NoMatch/NoMatch.jsx
+++ src/pages/NoMatch/NoMatch.jsx
@@ -1,7 +1,20 @@
-export default function NoMatch(props) {
+import { Link } from "react-router-dom";
+import "./NoMatch.scss";
+
+export default function NoMatch() {
   return (
-    <div className="product">
-      <p>404! Cette page n'existe pas</p>
+    <div className="wrapper-error">
+      <div className="wrapper-error__number">404</div>
+      <div className="wrapper-error__msg">
+        Oups! La page que vous demandez n'existe pas.
+      </div>
+      <Link
+        className="wrapper-error__back-link"
+        to="/"
+        title="Retour page d'accueil"
+      >
+        Retourner sur la page d'accueil
+      </Link>
     </div>
   );
 }
